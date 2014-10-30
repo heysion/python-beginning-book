a6081c018c0507544b7f81f56a345234e156a7c0
Heysion <heysions@gmail.com>
2014-10-30 09:41:10 +0800

    chapter 06 function oo

---
+++ chapter-06/oo_function.py
@@ -0,0 +1,34 @@
+#
+def print_var(div,var):
+    print "%s = %s  %s" %(str(var),repr(div),repr(type(div)))
+
+def if_print(x):
+    if x :
+        print '%s %s == True' %(x,repr(type(x)))
+    else:
+        print '%s %s == False' %(x,repr(type(x)))
+#
+#keyword function
+def func1(name,greeting="Hello"):
+    print "%s ,%s !" %(greeting,name)
+#
+func1(greeting = 'Good night',name = "Heysion")
+func1(name = "Heysion")
+exit()
+#keyword fib function
+def fibs(num):
+    'fib function'
+    assert num > 2
+    result = [0,1]
+    for i in range(num-2):
+        result.append(result[-2] + result[-1])
+    return result
+#
+print fibs(10)
+print fibs.__doc__
+print fibs(1)
+exit()
+#keyword callable
+print callable(print_var)
+x = 1
+print callable(x)
